b6a3d86edcd9736d23771184193a0e098ed96f96
Prashant Kumar Maurya <45981671+prashant4471@users.noreply.github.com>
2020-06-20 15:04:01 +0530

    Create PINS.cpp

---
+++ June/PINS.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long int n;
+        cin>>n;
+        long int a,res;
+        a=n/2;
+        cout<<"1 1";
+        for(long int i=0;i<a;i++)
+        cout<<"0";
+        cout<<"\n";
+    }
+    return 0;
+}
